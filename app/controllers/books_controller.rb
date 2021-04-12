c4dd74cb9a8b369346b81cf5b72031334e9cf320
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-12 02:13:45 +0000

    [Update] bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def create
     book = Book.new(book_params)
     book.save
@@ -19,9 +19,19 @@ class BooksController < ApplicationController
 
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 root to: "homes#top"
 resources :books
 post "books" => "books#create"
-get "books/:id" => "books#show", as: "todolist"
+get "books/:id" => "books#show", as: "booklists"
+get "books/:id/edit" => "books#edit", as: "edit_booklists"
+patch "books/:id" => "books#update", as: "update_booklists"
+delete "books/:id" => "books#destroy", as: "destroy_booklists"
+
 
 
 end
